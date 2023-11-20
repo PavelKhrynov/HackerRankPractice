3a862715a0320f30f502dcf3b8439e73bfed4c76
Pavel Khrynov <darkpreacher@live.ru>
2023-11-20 20:16:47 +0300

    Library Fine Problem

--- HackerRankTasks/Program.cs
+++ HackerRankTasks/Program.cs
@@ -9,13 +9,25 @@ namespace HackerRankTasks
     {
         static void Main(string[] args)
         {
-            int T = Convert.ToInt32(Console.ReadLine().Trim());
-            for (int TItr = 0; TItr < T; TItr++)
-            {
-                string w = Console.ReadLine();
-                string result = BiggerIsGreater.SolveProblim(w);
-                Console.WriteLine(result);
-            }
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int d1 = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m1 = Convert.ToInt32(firstMultipleInput[1]);
+
+            int y1 = Convert.ToInt32(firstMultipleInput[2]);
+
+            string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int d2 = Convert.ToInt32(secondMultipleInput[0]);
+
+            int m2 = Convert.ToInt32(secondMultipleInput[1]);
+
+            int y2 = Convert.ToInt32(secondMultipleInput[2]);
+
+            int result = LibraryFine.SolveProblem(d1, m1, y1, d2, m2, y2);
+
+            Console.WriteLine(result);
         }
     }
 }
