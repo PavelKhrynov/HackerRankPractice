c708bfe5eabbaea451bbd1436a679f8c410ce87a
Pavel Khrynov <darkpreacher@live.ru>
2023-11-26 21:20:01 +0300

    Problems:
    # TaumAndBday
    # CamelCase

--- HackerRankTasks/Program.cs
+++ HackerRankTasks/Program.cs
@@ -9,25 +9,28 @@ namespace HackerRankTasks
     {
         static void Main(string[] args)
         {
-            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            int t = Convert.ToInt32(Console.ReadLine().Trim());
 
-            int d1 = Convert.ToInt32(firstMultipleInput[0]);
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-            int m1 = Convert.ToInt32(firstMultipleInput[1]);
+                int b = Convert.ToInt32(firstMultipleInput[0]);
 
-            int y1 = Convert.ToInt32(firstMultipleInput[2]);
+                int w = Convert.ToInt32(firstMultipleInput[1]);
 
-            string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+                string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-            int d2 = Convert.ToInt32(secondMultipleInput[0]);
+                int bc = Convert.ToInt32(secondMultipleInput[0]);
 
-            int m2 = Convert.ToInt32(secondMultipleInput[1]);
+                int wc = Convert.ToInt32(secondMultipleInput[1]);
 
-            int y2 = Convert.ToInt32(secondMultipleInput[2]);
+                int z = Convert.ToInt32(secondMultipleInput[2]);
 
-            int result = LibraryFine.SolveProblem(d1, m1, y1, d2, m2, y2);
+                long result = TaumAndBday.SolveProblem(b, w, bc, wc, z);
 
-            Console.WriteLine(result);
+                Console.WriteLine(result);
+            }
         }
     }
 }
---
+++ HackerRankTasks/Tasks/TaumAndBday.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTasks.Tasks
+{
+    /*
+        Taum is planning to celebrate the birthday of his friend, Diksha.
+        There are two types of gifts that Diksha wants from Taum: one is black and the other is white.
+        To make her happy, Taum has to buy b black gifts and w white gifts.
+        - The cost of each black gift is bc units.
+        - The cost of every white gift is wc units.
+        - The cost to convert a black gift into white gift or vice versa is z units.
+        Determine the minimum cost of Diksha's gifts.
+     */
+    internal class TaumAndBday
+    {
+        public static long SolveProblem(long b, long w, long bc, long wc, long z)
+        {
+            long firstVariant = b * bc + w * wc;
+            long secondVariant = b * (wc + z) + w * wc;
+            long thirsVariant = b * bc + w * (bc + z);
+            long forthVariant = b * (wc + z) + w * (bc + z);
+
+            return Math.Min(Math.Min(Math.Min(firstVariant, secondVariant), thirsVariant), forthVariant);
+        }
+    }
+}
